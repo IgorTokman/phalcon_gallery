58e37ef2a179feec53849612289c28e326f4d179
IgorTokman <igortokman1994@ukr.net>
2016-02-28 21:04:55 +0400

    realized first page of admin room

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class AdminController extends Controller
+{
+    public function initialize(){
+        $this->assets
+            ->collection('header')
+            ->setTargetPath('css/style.min.css')
+            ->setTargetUri('css/style.min.css')
+            ->addCss('libs/Bootstrap/css/bootstrap.min.css')
+            ->join(true)
+            ->addFilter(new Phalcon\Assets\Filters\Cssmin());
+
+        $this->assets
+            ->collection('footer')
+            ->setTargetPath('js/style.min.js')
+            ->setTargetUri('js/style.min.js')
+            ->addJs('libs/jquery/jquery-1.11.1.min.js')
+            ->addJs('libs/Bootstrap/js/bootstrap.min.js')
+            ->join(true)
+            ->addFilter(new Phalcon\Assets\Filters\Jsmin());
+    }
+
+    public function managementAction(){
+        $this->view->setVar('images', Images::find());
+    }
+}
